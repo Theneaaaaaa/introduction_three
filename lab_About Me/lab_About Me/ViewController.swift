b9991c4aac8be71a851a7eb2c71c347f4dea4812
Thenea <thenea@iMac4.local>
2019-05-02 11:01:49 -0400

    finished

---
+++ lab_About Me/lab_About Me/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lab_About Me
+//
+//  Created by Thenea on 5/1/19.
+//  Copyright © 2019 Thenea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
